a8d93d256076faaa3939e5bffcc45d132a4b6bf8
Morteza.N <morteza.n@behinrahkar.com>
2025-06-30 09:44:12 +0330

    refactor DiscoveryAdapterCard

--- src/builtinApps/AssetIdentificationApp/DiscoverySettings/components/DiscoveryAdapters/components/DiscoveryAdapterCard/index.tsx
+++ src/builtinApps/AssetIdentificationApp/DiscoverySettings/components/DiscoveryAdapters/components/DiscoveryAdapterCard/index.tsx
@@ -16,69 +16,69 @@ type Props = EachAdapterConfiguration & {
 
 const DiscoveryAdapterCard = (props: Props) => {
 	const [editMode, setEditMode] = useState(false);
+
+	if (editMode) {
+		return (
+			<DiscoveryAdaptersForm
+				adapterId={props.adapterId}
+				loading={props.loading}
+				config={props.config}
+				handleEditAdapterConfigurations={props.handleEditAdapterConfigurations}
+				onCancel={() => setEditMode(false)}
+			/>
+		);
+	}
 	return (
-		<>
-			{editMode ? (
-				<DiscoveryAdaptersForm
-					adapterId={props.adapterId}
-					loading={props.loading}
-					config={props.config}
-					handleEditAdapterConfigurations={props.handleEditAdapterConfigurations}
-					onCancel={() => setEditMode(false)}
-				/>
-			) : (
-				<Card bg="gray.1" w="100%" padding="xs">
-					<Flex align="center" justify="space-between">
-						<Text fw="bold" fz="sm">
-							{`${props.config?.ip} - ${props.config?.connection}`}
+		<Card bg="gray.1" w="100%" padding="xs">
+			<Flex align="center" justify="space-between">
+				<Text fw="bold" fz="sm">
+					{`${props.config?.ip} - ${props.config?.connection}`}
+				</Text>
+				<Flex gap="2xs">
+					<Badge variant="light" color={props.is_active ? "green" : "red"} p="sm">
+						<Text p="2xs" tt="capitalize">
+							{props.is_active ? "Connected" : "Disconnected"}
 						</Text>
-						<Flex gap="2xs">
-							<Badge variant="light" color={props.is_active ? "green" : "red"} p="sm">
-								<Text p="2xs" tt="capitalize">
-									{props.is_active ? "Connected" : "Disconnected"}
-								</Text>
-							</Badge>
-							<ActionIcon
-								// onClick={() => form.removeListItem("gateways", index)}
-								title="Test Connection"
-								variant="subtle"
-								c="gray.8"
-							>
-								<IconPlugConnected size={20} />
+					</Badge>
+					<ActionIcon
+						// onClick={() => form.removeListItem("gateways", index)}
+						title="Test Connection"
+						variant="subtle"
+						c="gray.8"
+					>
+						<IconPlugConnected size={20} />
+					</ActionIcon>
+					<ActionIcon
+						// onClick={() => form.removeListItem("gateways", index)}
+						title="View Results"
+						variant="subtle"
+						c="gray.8"
+					>
+						<IconListDetails size={20} />
+					</ActionIcon>
+					<ActionIcon
+						onClick={() => setEditMode((perValue) => !perValue)}
+						title="Edit"
+						variant="subtle"
+						c="gray.8"
+					>
+						<IconPencil size={20} />
+					</ActionIcon>
+					<BCPopoverConfirm
+						loading={props.loading}
+						onConfirm={props.handleDeleteAdapterConfigurations}
+						confirmBtnColor="red"
+						confirmBtnText="Delete"
+						message="Are you shure to delete gateway ?"
+						renderProps={(onToggle) => (
+							<ActionIcon onClick={onToggle} title="Delete" variant="subtle" c="gray.8">
+								<IconX size={20} />
 							</ActionIcon>
-							<ActionIcon
-								// onClick={() => form.removeListItem("gateways", index)}
-								title="View Results"
-								variant="subtle"
-								c="gray.8"
-							>
-								<IconListDetails size={20} />
-							</ActionIcon>
-							<ActionIcon
-								onClick={() => setEditMode((perValue) => !perValue)}
-								title="Edit"
-								variant="subtle"
-								c="gray.8"
-							>
-								<IconPencil size={20} />
-							</ActionIcon>
-							<BCPopoverConfirm
-								loading={props.loading}
-								onConfirm={props.handleDeleteAdapterConfigurations}
-								confirmBtnColor="red"
-								confirmBtnText="Delete"
-								message="Are you shure to delete gateway ?"
-								renderProps={(onToggle) => (
-									<ActionIcon onClick={onToggle} title="Delete" variant="subtle" c="gray.8">
-										<IconX size={20} />
-									</ActionIcon>
-								)}
-							/>
-						</Flex>
-					</Flex>
-				</Card>
-			)}
-		</>
+						)}
+					/>
+				</Flex>
+			</Flex>
+		</Card>
 	);
 };
 
