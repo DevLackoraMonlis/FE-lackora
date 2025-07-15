95b9c3da6e6e46f68ffbef5455439ea231ffb85b
Farshid <farenjir@gmail.com>
2025-07-15 11:06:07 +0330

    update-orval

--- src/builtinApps/AdaptersApp/index.helper.ts
+++ src/builtinApps/AdaptersApp/index.helper.ts
@@ -1,11 +1,41 @@
 import { AdapterUploadedStatus } from "./index.enum";
 
-export const handleGetStatusFromResponse = (subTitle: string) => {
-	const status = subTitle?.includes("older")
-		? AdapterUploadedStatus.Downgrade
-		: subTitle?.includes("exist")
-			? AdapterUploadedStatus.Exists
-			: AdapterUploadedStatus.Error;
-
-	return status;
+type AdapterValidationResponse = {
+	status: boolean;
+	type?: "icon_not_found" | "multiple_icon" | "old_version" | null;
+	message: string;
 };
+
+export function handleGetStatusFromResponse<T extends AdapterValidationResponse>(data?: T) {
+	const { type, message = "", status: apiStatus = false } = data || {};
+
+	let apiType = type;
+	if (!apiStatus) apiType = null;
+
+	let status: AdapterUploadedStatus;
+	let subTitle = "";
+
+	switch (apiType) {
+		case "icon_not_found":
+			status = AdapterUploadedStatus.Ready;
+			subTitle = message;
+			break;
+
+		case "multiple_icon":
+			status = AdapterUploadedStatus.Exists;
+			subTitle = message;
+			break;
+
+		case "old_version":
+			status = AdapterUploadedStatus.Downgrade;
+			subTitle = message;
+			break;
+
+		default:
+			status = AdapterUploadedStatus.Error;
+			subTitle = message || AdapterUploadedStatus.Error;
+			break;
+	}
+
+	return { status, subTitle, apiStatus };
+}
--- src/builtinApps/AssetIdentificationApp/DiscoverySettings/components/DiscoveryAdapters/index.tsx
+++ src/builtinApps/AssetIdentificationApp/DiscoverySettings/components/DiscoveryAdapters/index.tsx
@@ -37,7 +37,6 @@ export default function DiscoverySettingsDiscoveryAdapters() {
 
 	return (
 		<Grid p="sm" pt="lg" gutter="lg">
-			<LoadingOverlay visible={discoveryAdapters.isLoading} />
 			<Grid.Col span={3}>
 				<BCSideFilter
 					height={height - 300}
@@ -53,7 +52,12 @@ export default function DiscoverySettingsDiscoveryAdapters() {
 					]}
 				/>
 			</Grid.Col>
-			<Grid.Col span={9} style={{ overflowY: "auto", overflowX: "hidden", height: height - 150 }}>
+			<Grid.Col
+				span={9}
+				style={{ overflowY: "auto", overflowX: "hidden", height: height - 150 }}
+				pos="relative"
+			>
+				<LoadingOverlay visible={discoveryAdapters.isLoading} />
 				<Accordion variant="separated" onChange={setActiveAccordion}>
 					{discoveryAdapters?.data?.results?.map((item) => (
 						<Accordion.Item key={item.id} value={item.id}>
