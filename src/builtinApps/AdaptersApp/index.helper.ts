4622258490c99359a2070044fcf4cc1e0e2866ad
Farshid <farenjir@gmail.com>
2025-07-15 11:29:08 +0330

    fix-and-debug-adapter-types

--- src/builtinApps/AdaptersApp/index.helper.ts
+++ src/builtinApps/AdaptersApp/index.helper.ts
@@ -2,32 +2,48 @@ import { AdapterUploadedStatus } from "./index.enum";
 
 type AdapterValidationResponse = {
 	status: boolean;
-	type?: "icon_not_found" | "multiple_icon" | "old_version" | null;
 	message: string;
+	type?:
+		| "exists"
+		| "adapter_json_invalid"
+		| "upgrade"
+		| "ready"
+		| "installed"
+		| "icon_not_found"
+		| "multiple_icon"
+		| "old_version"
+		| null;
 };
 
 export function handleGetStatusFromResponse<T extends AdapterValidationResponse>(data?: T) {
 	const { type, message = "", status: apiStatus = false } = data || {};
 
-	let apiType = type;
-	if (!apiStatus) apiType = null;
-
 	let status: AdapterUploadedStatus;
-	let subTitle = "";
+	let subTitle: string;
 
-	switch (apiType) {
-		case "icon_not_found":
+	switch (type) {
+		case "ready":
 			status = AdapterUploadedStatus.Ready;
 			subTitle = message;
 			break;
 
-		case "multiple_icon":
+		case "exists":
 			status = AdapterUploadedStatus.Exists;
+			subTitle = message;
+			break;
+
+		case "upgrade":
+			status = AdapterUploadedStatus.Upgrade;
 			subTitle = message;
 			break;
 
 		case "old_version":
 			status = AdapterUploadedStatus.Downgrade;
+			subTitle = message;
+			break;
+
+		case "adapter_json_invalid":
+			status = AdapterUploadedStatus.Invalid;
 			subTitle = message;
 			break;
 
