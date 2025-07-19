c613a911352d535fb6374263d29ec387973751ac
Morteza.N <morteza.n@behinrahkar.com>
2025-07-19 19:20:58 +0330

    update artifacts

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,10 @@ import { defineConfig, devices } from "@playwright/test";
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+// playwright.config.ts
+const isCI = !!process.env.CI; // در GitLab یا هر CI دیگر
+const isProd = process.env.NODE_ENV === "production";
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -17,9 +21,9 @@ export default defineConfig({
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	forbidOnly: !!process.env.CI,
+	forbidOnly: isCI,
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	retries: isCI ? 2 : 0,
 	/* Opt out of parallel tests on CI. */
 	workers: 1,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -28,7 +32,7 @@ export default defineConfig({
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		baseURL: "http://localhost:3000",
-		headless: !!process.env.CI,
+		headless: isCI,
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
 		actionTimeout: 10000,
@@ -66,9 +70,9 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: process.env.NODE_ENV !== "production" ? "npm run dev" : "node .next/standalone/server.js",
+		command: !isProd ? "npm run dev" : "node .next/standalone/server.js",
 		url: "http://localhost:3000",
-		reuseExistingServer: !process.env.CI,
+		reuseExistingServer: !isCI,
 		timeout: 60 * 3 * 1000, // 2 دقیقه صبر می‌کنه تا سرور بالا بیاد
 	},
 });
