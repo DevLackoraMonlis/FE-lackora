eee0e505aa44a6ba69f0d0db10e148c64c4dafcb
Morteza.N <morteza.n@behinrahkar.com>
2025-07-21 11:43:06 +0330

    add mono market header

--- src/app/[locale]/panel/mono-market/page.tsx
+++ src/app/[locale]/panel/mono-market/page.tsx
@@ -1,5 +1,10 @@
 import ICMonoMarket from "@/shared/components/infraComponents/ICMonoMarket/components/ICMonoMarket";
+import ICPageWrapper from "@/shared/components/infraComponents/ICPageWrapper";
 
 export default function MonoMarketPage() {
-	return <ICMonoMarket />;
+	return (
+		<ICPageWrapper title={"Mono Market"}>
+			<ICMonoMarket />
+		</ICPageWrapper>
+	);
 }
