3b2eb4e06b5d75ba023f38cf6269277eda75a1b2
farshid.e <farshid.e@houshafzar.ir>
2025-08-12 14:52:37 +0330

    init-[{} as FormValues[conditions][number]]

--- src/shared/components/baseComponents/BCTriggerActions/index.tsx
+++ src/shared/components/baseComponents/BCTriggerActions/index.tsx
@@ -8,129 +8,128 @@ import { useIconPolicyManagementActions, usePolicyManagementActions } from "./in
 import type { TriggerActionForm, TriggerActionFormList, TriggerActionIcons } from "./index.types";
 
 type SelectOptionProps = {
-  label: string;
-  description: string;
-  disabled: boolean;
-  iconType?: TriggerActionIcons;
+	label: string;
+	description: string;
+	disabled: boolean;
+	iconType?: TriggerActionIcons;
 };
 
 function SelectOption({ iconType, label, description, disabled }: SelectOptionProps) {
-  return (
-    <Card m={0} p={0}>
-      <Flex gap="md" p="xs" bg={disabled ? "gray.3" : "gray.1"}>
-        <Flex justify="center" align="center" fz="h1" bg="transparent">
-          {iconType ? createElement(iconType) : <IconZoomScan />}
-        </Flex>
-        <Flex direction="column">
-          <Text fz="sm">{label}</Text>
-          <Text fz="xs" c="dimmed">
-            {description}
-          </Text>
-        </Flex>
-      </Flex>
-    </Card>
-  );
+	return (
+		<Card m={0} p={0}>
+			<Flex gap="md" p="xs" bg={disabled ? "gray.3" : "gray.1"}>
+				<Flex justify="center" align="center" fz="h1" bg="transparent">
+					{iconType ? createElement(iconType) : <IconZoomScan />}
+				</Flex>
+				<Flex direction="column">
+					<Text fz="sm">{label}</Text>
+					<Text fz="xs" c="dimmed">
+						{description}
+					</Text>
+				</Flex>
+			</Flex>
+		</Card>
+	);
 }
 
 export default function BCTriggerActions<T extends TriggerActionForm>({
-  values,
-  onChange,
+	values,
+	onChange,
 }: {
-  values: T;
-  onChange: (values: T) => void;
+	values: T;
+	onChange: (values: T) => void;
 }) {
-  const updateValueByDependency = useRef<TriggerActionFormList>({});
-  const form = useForm<T>({
-    onValuesChange: (values) => {
-      const dependencyValue = updateValueByDependency.current;
-      Object.entries(dependencyValue).forEach(([key, value]) => {
-        form.setFieldValue(key, value as never);
-      });
-      Object.assign(values, dependencyValue);
-      onChange(values);
-    },
-  });
+	const updateValueByDependency = useRef<TriggerActionFormList>({});
+	const form = useForm<T>({
+		onValuesChange: (values) => {
+			const dependencyValue = updateValueByDependency.current;
+			Object.entries(dependencyValue).forEach(([key, value]) => {
+				form.setFieldValue(key, value as never);
+			});
+			Object.assign(values, dependencyValue);
+			onChange(values);
+		},
+	});
 
-  const triggerActions = Object.keys(values);
-  const { getPolicyActionIcon } = useIconPolicyManagementActions();
-  const combobox = useCombobox({
-    onDropdownClose: () => combobox.resetSelectedOption(),
-  });
+	const triggerActions = Object.keys(values);
+	const { getPolicyActionIcon } = useIconPolicyManagementActions();
+	const combobox = useCombobox({
+		onDropdownClose: () => combobox.resetSelectedOption(),
+	});
 
-  const { policyActions } = usePolicyManagementActions();
-  const options = Object.entries(policyActions?.data || {}).map(([groupName, list]) => (
-    <Fragment key={groupName}>
-      <Text c="dimmed" py="xs" px="2x">
-        {groupName}
-      </Text>
-      {list?.map((item) => {
-        const key = `${groupName}|${item.name}|${item.id}`;
-        const disabled = triggerActions.includes(key);
-        return (
-          <Combobox.Option key={key} value={key} m={0} p="3xs" disabled={disabled}>
-            <SelectOption
-              iconType={getPolicyActionIcon(item.name)}
-              label={item.display_name}
-              description={item.description}
-              disabled={disabled}
-            />
-          </Combobox.Option>
-        );
-      })}
-    </Fragment>
-  ));
+	const { policyActions } = usePolicyManagementActions();
+	const options = Object.entries(policyActions?.data || {}).map(([groupName, list]) => (
+		<Fragment key={groupName}>
+			<Text c="dimmed" py="xs" px="2x">
+				{groupName}
+			</Text>
+			{list?.map((item) => {
+				const key = `${groupName}|${item.name}|${item.id}`;
+				const disabled = triggerActions.includes(key);
+				return (
+					<Combobox.Option key={key} value={key} m={0} p="3xs" disabled={disabled}>
+						<SelectOption
+							iconType={getPolicyActionIcon(item.name)}
+							label={item.display_name}
+							description={item.description}
+							disabled={disabled}
+						/>
+					</Combobox.Option>
+				);
+			})}
+		</Fragment>
+	));
 
-  console.log({ values });
-  useEffect(() => {
-    if (triggerActions?.length) {
-      form.setValues(values);
-    }
-  }, [`${triggerActions}`]);
-	
-  return (
-    <>
-      {triggerActions.map((triggerAction = "") => (
-        <TriggerActionGenerator<T>
-          key={triggerAction}
-          {...{
-            triggerActions,
-            updateValueByDependency,
-            triggerAction,
-            form,
-            values,
-          }}
-        />
-      ))}
-      <Combobox
-        withinPortal
-        store={combobox}
-        onOptionSubmit={(val) => {
-          form.setFieldValue(val, [] as never);
-          combobox.closeDropdown();
-        }}
-      >
-        <Combobox.Target>
-          <Flex w="500px" mt="sm">
-            <Button
-              loading={policyActions.isLoading}
-              variant="transparent"
-              rightSection={<IconChevronCompactDown size={15} />}
-              leftSection={<IconPlus size={15} />}
-              onClick={() => combobox.toggleDropdown()}
-              size="xs"
-            >
-              Add actions
-            </Button>
-          </Flex>
-        </Combobox.Target>
-        <Combobox.Dropdown>
-          <Combobox.Options style={{ maxHeight: 400, overflow: "auto" }}>
-            <Flex direction="column" px="sm" pb="xs">
-              {options}
-            </Flex>
-          </Combobox.Options>
-        </Combobox.Dropdown>
-      </Combobox>
-    </>
-  );
+	useEffect(() => {
+		if (triggerActions?.length) {
+			form.setValues(values);
+		}
+	}, [`${triggerActions}`]);
+
+	return (
+		<>
+			{triggerActions.map((triggerAction = "") => (
+				<TriggerActionGenerator<T>
+					key={triggerAction}
+					{...{
+						triggerActions,
+						updateValueByDependency,
+						triggerAction,
+						form,
+						values,
+					}}
+				/>
+			))}
+			<Combobox
+				withinPortal
+				store={combobox}
+				onOptionSubmit={(val) => {
+					form.setFieldValue(val, [] as never);
+					combobox.closeDropdown();
+				}}
+			>
+				<Combobox.Target>
+					<Flex w="500px" mt="sm">
+						<Button
+							loading={policyActions.isLoading}
+							variant="transparent"
+							rightSection={<IconChevronCompactDown size={15} />}
+							leftSection={<IconPlus size={15} />}
+							onClick={() => combobox.toggleDropdown()}
+							size="xs"
+						>
+							Add actions
+						</Button>
+					</Flex>
+				</Combobox.Target>
+				<Combobox.Dropdown>
+					<Combobox.Options style={{ maxHeight: 400, overflow: "auto" }}>
+						<Flex direction="column" px="sm" pb="xs">
+							{options}
+						</Flex>
+					</Combobox.Options>
+				</Combobox.Dropdown>
+			</Combobox>
+		</>
+	);
 }
