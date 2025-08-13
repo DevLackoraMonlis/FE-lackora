eba55768d2bf55bf3ee5c15c3698d3aa85def6e1
farshid.e <farshid.e@houshafzar.ir>
2025-08-13 12:33:18 +0330

    ip-blue

--- src/builtinApps/AssetIdentificationApp/DiscoverySettings/components/DiscoveryAdapters/components/DeleteDiscoveryAdapter/components/DeleteAssetsDependencyTable/index.tsx
+++ src/builtinApps/AssetIdentificationApp/DiscoverySettings/components/DiscoveryAdapters/components/DeleteDiscoveryAdapter/components/DeleteAssetsDependencyTable/index.tsx
@@ -50,7 +50,7 @@ export function DeleteAssetsDependencyTable({ results = [] }: Props) {
 							<Divider />
 							<Grid px="xs" py="2xs">
 								<Grid.Col span={8}>
-									<Flex gap="xl">
+									<Flex gap="sm">
 										<Text>{`${key}.`}</Text>
 										<Highlight highlight={[search]} highlightStyles={{}}>
 											{`${hostname} - ${ipAddress}`}
