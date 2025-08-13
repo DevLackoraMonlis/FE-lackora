6296abf32279638920881b22dc9fe3e1a659bd39
Morteza.N <morteza.n@behinrahkar.com>
2025-08-13 08:59:56 +0330

    fix inventory

--- src/builtinApps/InventoryApp/components/InventoryAppPageWrapper/index.tsx
+++ src/builtinApps/InventoryApp/components/InventoryAppPageWrapper/index.tsx
@@ -1,26 +1,70 @@
 import InventoryAppSideCardItem from "@/builtinApps/InventoryApp/components/InventoryAppSideCardItem";
 import type { InventoryAppPageWrapperProps } from "@/builtinApps/InventoryApp/index.types";
-import { Flex, Grid, Text } from "@mantine/core";
+import { Flex, Grid, ScrollArea, Text } from "@mantine/core";
+import { useViewportSize } from "@mantine/hooks";
 
 export default function InventoryAppPageWrapper(props: InventoryAppPageWrapperProps) {
+	const { height } = useViewportSize();
 	return (
 		<Grid gutter={0} h={"100%"} classNames={{ inner: "h-full" }}>
 			<Grid.Col span={3}>
-				<Flex direction={"column"} h="100%" bg={"gray.2"} gap={"xs"} px={"lg"} py={"sm"}>
-					<Flex pl={"xs"} align={"center"} bg={"gray.2"} h={48}>
-						<Text fz={"lg"} fw={"bolder"}>
-							{props.title}
-						</Text>
+				<ScrollArea scrollbarSize={2} scrollbars={"y"} bg={"gray.2"} h={height - 50}>
+					<Flex direction={"column"} h="100%" gap={"xs"} px={"lg"} py={"sm"}>
+						<Flex pl={"xs"} align={"center"} bg={"gray.2"} h={48}>
+							<Text fz={"lg"} fw={"bolder"}>
+								{props.title}
+							</Text>
+						</Flex>
+						{props.sideItems.map((item) => (
+							<InventoryAppSideCardItem
+								key={item.name}
+								isSelected={props.selectedInventoryType === item.name}
+								isLoading={props.isLoading}
+								{...item}
+							/>
+						))}
+						{props.sideItems.map((item) => (
+							<InventoryAppSideCardItem
+								key={item.name}
+								isSelected={props.selectedInventoryType === item.name}
+								isLoading={props.isLoading}
+								{...item}
+							/>
+						))}
+						{props.sideItems.map((item) => (
+							<InventoryAppSideCardItem
+								key={item.name}
+								isSelected={props.selectedInventoryType === item.name}
+								isLoading={props.isLoading}
+								{...item}
+							/>
+						))}
+						{props.sideItems.map((item) => (
+							<InventoryAppSideCardItem
+								key={item.name}
+								isSelected={props.selectedInventoryType === item.name}
+								isLoading={props.isLoading}
+								{...item}
+							/>
+						))}
+						{props.sideItems.map((item) => (
+							<InventoryAppSideCardItem
+								key={item.name}
+								isSelected={props.selectedInventoryType === item.name}
+								isLoading={props.isLoading}
+								{...item}
+							/>
+						))}
+						{props.sideItems.map((item) => (
+							<InventoryAppSideCardItem
+								key={item.name}
+								isSelected={props.selectedInventoryType === item.name}
+								isLoading={props.isLoading}
+								{...item}
+							/>
+						))}
 					</Flex>
-					{props.sideItems.map((item) => (
-						<InventoryAppSideCardItem
-							key={item.name}
-							isSelected={props.selectedInventoryType === item.name}
-							isLoading={props.isLoading}
-							{...item}
-						/>
-					))}
-				</Flex>
+				</ScrollArea>
 			</Grid.Col>
 			<Grid.Col span={9}>{props.page}</Grid.Col>
 		</Grid>
